829817974868df6b2f418b94a485d5fd1ffc6d61
lesnykhvadim999-beep <lesnykhvadim999@gmail.com>
2026-05-15 17:23:49 +0400

    Update sales_scraper.py

--- sales_scraper.py
+++ sales_scraper.py
@@ -7,7 +7,7 @@ from psycopg2.extras import execute_values
 DATABASE_URL = os.getenv("DATABASE_URL")
 API_URL = "[https://gateway.dubailand.gov.ae/open-data/transactions](https://gateway.dubailand.gov.ae/open-data/transactions)"
 
-if not DATABASE_URL:
+    if not DATABASE_URL:
 raise RuntimeError("DATABASE_URL is not set")
 
 conn = psycopg2.connect(DATABASE_URL)
